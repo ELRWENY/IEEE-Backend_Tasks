6172c67e82ab857a9decc89f8bc2afd007613a06
Ahmed anwer elrweny <146839312+ELRWENY@users.noreply.github.com>
2024-11-15 00:26:37 +0200

    Add files via upload

---
+++ task_2_v1/TASK/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TASK
+{
+    public class Main_Programe
+    {
+        static void Main(string[] args)
+        { 
+            Contacts.Start();
+            Console.ReadKey();
+        }
+
+    }
+}
